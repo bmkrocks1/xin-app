2e371cb5c45c36a3f581c6e1cbf2090d0ca507dd
Billie Ko <bmkrocks@gmail.com>
2017-02-16 23:37:12 +0800

    drag and drop

--- mockups/app.js
+++ mockups/app.js
@@ -1,87 +1,156 @@
 'use strict';
 
 $(document).ready(function() {
+  var colorChooserDropdown = $('#color-chooser-dropdown'),
+      heightLimit = 200,
+      cardTitle = $('.card-title');
 
   $('.card').click(function(event) {
     event.stopPropagation();
 
-    $('.card-actions').removeClass('active');
-    $('#color-chooser-dropdown').hide();
+    hideCardActions();
+    hideColorChooserDropdown();
 
-    var card = $(this);
     if ($(event.target).is('.card-title')) {
-      card.addClass('active');
+      $(this).addClass('active');
     }
   });
 
-  var heightLimit = 200,
-      cardTitle = $('.card-title');
-
-  // on enter
+  /*
+   * .card-title onEnter and focus out handler
+   */
   cardTitle
     .keypress(function(event) {
-      var ta = $(this);
       if (event.which == 13) {
-        ta.blur();
+        $(this).blur();
       }
     })
     .focusout(function() {
         $(this).parent().removeClass('active');
     });
 
+  // Add oninput handler to all .card-title
   cardTitle.each(function() {
-      this.oninput = adjustScrollHeight;
+      this.oninput = updateScrollHeight;
   });
 
-  // toggle color-chooser drop down
+  /*
+   * Toggle the Change color drop down
+   */
   $('.color-chooser').click(function(event) {
     event.stopPropagation();
 
     var cc = $(this),
-        placement = cc.data('placement'),
-        os = cc.offset(),
-        dropdown = $('#color-chooser-dropdown');
+        placement = cc.attr('data-placement'),
+        os = cc.offset();
 
-    dropdown.removeClass('placement-left');
-    $('.card-actions').removeClass('active');
+    removeOpenColorChooser(this);
+    hideCardActions();
+
+    colorChooserDropdown.removeClass('placement-left');
     cc.parent().addClass('active');
+
+    if (cc.hasClass('open')) {
+      cc.removeClass('open');
+      hideColorChooserDropdown();
+      return;
+    }
+
+    cc.addClass('open');
 
     var top = os.top + cc.height(),
         left;
 
     if (placement === 'left') {
-      dropdown.addClass('placement-left');
-      left = (os.left - dropdown.width()) + cc.width() + 17;
+      colorChooserDropdown.addClass('placement-left');
+      left = (os.left - colorChooserDropdown.width()) + cc.width() + 17;
     }
     else {
       left = os.left - 9;
     }
 
-    dropdown.css({ top: top + 'px', left: left + 'px' }).show();
+    colorChooserDropdown.css({ top: top + 'px', left: left + 'px' }).show();
   });
 
+  /*
+   * Click handler to any part of the page
+   */
   $(document).click(function() {
       $('.card').removeClass('active');
-      $('.card-actions').removeClass('active');
-      $('#color-chooser-dropdown').hide();
+      hideCardActions();
+      removeOpenColorChooser();
+      hideColorChooserDropdown();
   });
 
-
-  $(window).resize(onWindowResize);
-  onWindowResize();
-
+  /*
+   * Window resize handler
+   */
   function onWindowResize() {
     var h = $('body').height() - 115;
     $('.columns-container').height(h + 'px');
-    $('.card-title').each(adjustScrollHeight);
+    $('.card-title').each(updateScrollHeight);
   }
 
-  function adjustScrollHeight() {
+  function hideCardActions() {
+    $('.card-actions').removeClass('active');
+  }
+
+  /*
+   * Update the scrollHeight of .card-title
+   */
+  function updateScrollHeight() {
     var ta = $(this);
     if (this.scrollHeight > 30) {
       ta.height('');
       ta.height(Math.min(this.scrollHeight, heightLimit) + 'px');
     }
   }
+
+  /*
+   * Remove `open` class to .color-chooser buttons
+   */
+  function removeOpenColorChooser(except) {
+    $('.color-chooser.open').not(except).removeClass('open');
+  }
+
+  /*
+   * Hide the Change color drop down
+   */
+  function hideColorChooserDropdown() {
+    colorChooserDropdown.hide();
+  }
+
+  // -- Draggable
+  $('.card-draggable').draggable({
+    containment: '.columns-container',
+    revert: 'invalid',
+    zIndex: 100,
+    drag: function(event, ui) {
+        hideColorChooserDropdown();
+        removeOpenColorChooser();
+        ui.helper.parent().addClass('dragging-from');
+    }
+  });
+
+  // -- Droppable
+  $('.cards-container').droppable({
+    accept: '.card-draggable',
+    tolerance: 'intersect',
+    drop: function(event, ui) {
+      var dragged = ui.draggable;
+      dragged.parent().removeClass('dragging-from');
+      dragged.css({ left: '', top: ''});
+      $(this).append(dragged);
+      if (dragged.parent().is('#done')) {
+        dragged.find('.color-chooser').attr('data-placement', 'left');
+      }
+      else {
+        dragged.find('.color-chooser').attr('data-placement', 'right');
+      }
+    }
+  });
+
+  $(window).resize(onWindowResize);
+  onWindowResize();
 
 });
